e94e3876b9ad53824c234f1a8fa72dd34ba2aa98
Denisson Pereira Santos <denisson159@gmail.com>
2024-03-21 12:21:08 -0300

    init commit

---
+++ src/test/java/denisson/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package denisson.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
